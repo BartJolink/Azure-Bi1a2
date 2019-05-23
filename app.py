3f48e1ae1ee3d379670c4aebe618fa77139a3c92
BartJolink <43319904+BartJolink@users.noreply.github.com>
2019-05-23 13:52:57 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -507,7 +507,7 @@ function onClick(element) {{
 
 def execute_BLASTp(seq):
     result_handle = NCBIWWW.qblast("blastp", "nr", seq)
-    
+
     with open("my_blast.xml", "w") as out_handle:
         out_handle.write(result_handle.read())
     result_handle.close()
